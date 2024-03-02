9c6971f9428aa57430666e9a1278f353078329f5
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2024-03-02 16:37:23 +0300

    UICollectionViewCustomLayout

--- VkNewsFeed/Newsfeed/GalleryView/GalleryCollectionView.swift
+++ VkNewsFeed/Newsfeed/GalleryView/GalleryCollectionView.swift
@@ -12,20 +12,27 @@ class GalleryCollectionView: UICollectionView, UICollectionViewDelegate, UIColle
     var photos = [FeedCellPhotoAttachementViewModel]()
     
     init() {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        super.init(frame: .zero, collectionViewLayout: layout)
+        let rowLayout = RowLayout()
+        super.init(frame: .zero, collectionViewLayout: rowLayout)
         
         delegate = self
         dataSource = self
         
-        backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        backgroundColor = UIColor.white
+        
+        showsHorizontalScrollIndicator = false
+        showsVerticalScrollIndicator = false
         
         register(GalleryCollectionViewCell.self, forCellWithReuseIdentifier: GalleryCollectionViewCell.reuseId)
+        
+        if let rowLayout = collectionViewLayout as? RowLayout {
+            rowLayout.delegate = self
+        }
     }
     
     func set(photos: [FeedCellPhotoAttachementViewModel]) {
         self.photos = photos
+        contentOffset = CGPoint.zero
         reloadData()
     }
     
@@ -33,13 +40,28 @@ class GalleryCollectionView: UICollectionView, UICollectionViewDelegate, UIColle
         return photos.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = dequeueReusableCell(withReuseIdentifier: GalleryCollectionViewCell.reuseId, for: indexPath) as! GalleryCollectionViewCell
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+                        withReuseIdentifier: GalleryCollectionViewCell.reuseId,
+                        for: indexPath) as? GalleryCollectionViewCell else {
+            return UICollectionViewCell()
+        }
         cell.set(imageUrl: photos[indexPath.row].photoUrlString)
+
         return cell
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension GalleryCollectionView: RowLayoutDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, photoAtIndexPath indexPath: IndexPath) -> CGSize {
+        let width = photos[indexPath.row].width
+        let height = photos[indexPath.row].height
+        return CGSize(width: width, height: height)
     }
 }
--- VkNewsFeed/Newsfeed/GalleryView/GalleryCollectionViewCell.swift
+++ VkNewsFeed/Newsfeed/GalleryView/GalleryCollectionViewCell.swift
@@ -14,14 +14,13 @@ class GalleryCollectionViewCell: UICollectionViewCell {
     let myImageView: WebImageView = {
        let imageView = WebImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = #colorLiteral(red: 0.8901960784, green: 0.8980392157, blue: 0.9098039216, alpha: 1)
         return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
     
         addSubview(myImageView)
         // myImageView constraints
@@ -34,6 +33,16 @@ class GalleryCollectionViewCell: UICollectionViewCell {
     
     func set(imageUrl: String?) {
         myImageView.set(imageURL: imageUrl)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        myImageView.layer.masksToBounds = true
+        myImageView.layer.cornerRadius = 10
+        self.layer.shadowRadius = 3
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: 2.5, height: 4)
     }
     
     required init?(coder aDecoder: NSCoder) {
